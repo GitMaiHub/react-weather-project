7fbb733c6a0414a91681d1634bac1e422a502f25
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-10-13 12:03:03 -0600

    Added search engine to Weather.js.

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function Weather() {
     return (
         <div className="Weather">
+            <form>
+<div className="row">
+<div className="col-9">
+<input type="search" placeholder="Enter a city..." autoFocus={true} className="form-control" />
+</div>
+<div className="col-3">
+<input type="submit" value="Search" className="btn btn-primary" />
+</div>
 
+
+</div>
+            </form>
         </div>
     );
 }
