aa09054b95a6fd7af79c73c773e3324042017326
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-10-13 11:56:27 -0600

    Added Weather component and footer to App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+      <Weather />
+      </div>
+      <footer>
+        This project is coded by <a href="https://frolicking-dieffenbachia-8704e5.netlify.app/" title="Mai's portfolio" target="_blank" rel="noreferrer">Mai Carlton</a> and <a href="https://github.com/GitMaiHub/react-weather-project" title="Go to GitHub(external)" target="_blank" rel="noreferrer">open-sourced on GitHub</a> and <a href="https://app.netlify.com/sites/golden-mermaid-677844/overview" title="View this project on Netlify(external)" target="_blank" rel="noreferrer">hosted on Netlify</a>
+      </footer>
     </div>
   );
 }
